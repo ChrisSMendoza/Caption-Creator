c3e08aff38dbd2d524aa36c163b0dd9226724d3a
mendoza1 <csmendoza@ucdavis.edu>
2018-11-04 04:19:10 -0800

    catalog items are in a responsive 3cols by n-rows grid

--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-Vue.config.productionTip = false
+import Vuesax from 'vuesax';
+import 'vuesax/dist/vuesax.css' //Vuesax styles
+import 'material-icons/iconfont/material-icons.css'; // icons used by vuesax
+
+Vue.config.productionTip = false;
+
+Vue.use(Vuesax);
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,147 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  
+	<main class="full-vp">
+	
+		<!-- catalog header -->
+		<vs-row vs-justify="center" vs-align="center" vs-w="12">
+
+			<!-- logo, company name, section name -->
+			<vs-col class="ph-text-center " vs-w="4" vs-xs="12">
+				<h1>Apparel Pharaoh</h1>
+				<h4>Catalog</h4>
+			</vs-col>
+
+		</vs-row>
+
+
+		<!-- rows of apparel items -->
+		<vs-row 
+			vs-justify="space-around" 
+			vs-align="center"
+			vs-w="12"
+			type="flex"
+			class="catalog-wrapper">
+			
+			<vs-col 
+				v-for="item,index in catalogItems"
+				:key="index"
+
+				type="flex"
+				vs-xs="12" vs-sm="5" vs-lg="4"
+
+				class="catalog-col"
+			>
+				<vs-card actionable class="catalog-item" fixedHeight>
+
+					<div slot="header">
+						<p>{{item.name}}</p>
+					</div>
+
+					<div slot="media">
+						<img :src="item.imgSrc" class="catalog-item-img">
+					</div>
+
+					<div slot="footer">
+						<p>
+							{{item.price}}
+						</p>
+					</div>
+				</vs-card>
+
+			</vs-col>
+
+		</vs-row>
+
+	</main>
+
 </template>
 
+
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+
+	name: "Home",
+
+	data() {
+		return {
+
+			catalogItems: [
+				{
+					name: "Versace",
+					price: "$300",
+					imgSrc: "black_dress.png"
+				},
+				{
+					name: "Supreme",
+					price: "$60",
+					imgSrc: "suit.jpg"
+				},
+				{
+					name: "Gucci Belt",
+					price: "$150",
+					imgSrc: "tan_w_hat.png"
+				},
+				{
+					name: "Hanes Tee",
+					price: "$10",
+					imgSrc: "white_sweater.jpeg"
+				},
+				{
+					name: "Janowski",
+					price: "$55",
+					imgSrc: "pink_dress.png"
+				}
+			]
+		}
+	}
+
+};
+
 </script>
+
+<style>
+.catalog-wrapper {
+	/* background-color: lightgrey; */
+}
+.catalog-col {
+	padding: 15px;
+	height: 45vh;
+}
+.catalog-item {
+	margin: 0px;
+	padding: 5px;
+}
+.catalog-item-img {
+	height: 30vh;
+	object-fit: contain;
+}
+
+
+.ph-h-100 {
+	height: 100%;
+}
+.full-vp {
+	height: 100vh;
+}
+
+.ph-obj-fit-contain {
+	object-fit: contain;
+}
+
+.ph-text-center {
+    text-align: center;
+}
+.ph-mt-2 {
+	margin-top: 20px;
+}
+
+.ph-ms-2 {
+	margin-left: 20px;
+	margin-right: 20px;
+}
+
+.ph-vh-40 {
+	height: 40vh;
+}
+</style>
