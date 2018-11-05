1d6789746caba743816f8b90c77314fcdd0aaf86
mendoza1 <csmendoza@ucdavis.edu>
2018-11-04 23:34:38 -0800

    caption creator is defined and contains lyrics properly

--- src/views/CaptionCreator.vue
+++ src/views/CaptionCreator.vue
@@ -13,7 +13,7 @@
 
 export default {
 
-	name: "Home",
+	name: "CaptionCreator",
 
 	data() {
 		return {
--- tests/unit/CaptionCreater.spec.js
+++ tests/unit/CaptionCreater.spec.js
@@ -4,8 +4,37 @@ import { mount } from '@vue/test-utils';
 
 describe('CaptionCreator.vue', () => {
 
-	it('should split song lyrics into an array', () => {
+	const ccWrapper = mount(CaptionCreator);
 
-		const ccWrapper = mount(CaptionCreator);
-	})
+	it('should be created properly', () => {
+
+		expect(ccWrapper).toBeDefined(); // wrapper was set
+		expect(ccWrapper.vm).toBeDefined(); // the wrapper contains vue properties
+	});
+
+
+
+	it('should contain lyrics', () => {
+
+		let testLyrics = `
+				I'm on the pursuit of happiness, and I know
+				Everything that shine ain't always gonna be gold
+				Hey, I'll be fine once I get it
+				I'll be good
+				I'm on the pursuit of happiness, and I know
+				Everything that shine ain't always gonna be gold
+				Hey, I'll be fine once I get it
+				I'll be good
+			`
+			expect(ccWrapper.vm.lyrics).toBe(testLyrics);
+	});
+
+
+
+	it.skip('should split song lyrics into an array of lines', () => {
+
+	});
+
+
+
 });
