b3e5949c4837888793c0997835785cb4f1b47761
Chris Mendoza <realcsmendoza@gmail.com>
2019-03-10 16:41:37 -0700

    lyrics links variable in place, api data and http request lib imported

---
+++ src/components/StandsLyrics.vue
@@ -0,0 +1,34 @@
+<template>
+	
+</template>
+
+
+
+<script>
+import request from 'superagent'; // http requests, query strings
+
+import standsLyrics from '@/apis/StandsLyrics.js';
+
+export default {
+	
+	name: "StandsLyrics",
+
+	data() {
+		return {
+			lyricsLinks: [] // potential urls for the requested song lyrics
+		}
+	}
+
+	mount() {
+
+	}
+
+};
+</script>
+
+
+
+<style>
+	
+
+</style>
