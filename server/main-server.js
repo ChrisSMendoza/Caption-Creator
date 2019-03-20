756edd349e738b54bda1ea0a7f46706dcd5f93d1
Chris Mendoza <realcsmendoza@gmail.com>
2019-03-20 00:24:47 -0700

    scrape library added, GET '/' scrapes lyrics from static url

--- server/main-server.js
+++ server/main-server.js
@@ -5,12 +5,29 @@ const morgan = require('morgan');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const PORT = 8081; // since Vue dev server defaults to 8080
+const cheerio = require('cheerio');
+const request = require('request-promise'); // http request that returns a promise
+
+const PORT = 8081; // since vue dev server defaults to 8080
 const app = express();
 
 app.use(cors());
 app.use(morgan('tiny'));
 app.use(bodyParser.json({ type: 'application/json' }))
 
+app.get('/', (req, res) => {
 
+	const testUrl = "https://www.lyrics.com/lyric/185729";
+	
+	request(testUrl)
+		.then(html => {
+			console.log(html)
+
+			let lyrics = cheerio('#lyric-body-text', html);
+			res.send(lyrics.text());
+		})
+		.catch(err => console.log(err));
+});
+
+console.log(`listening on port ${PORT}`);
 app.listen(PORT);
