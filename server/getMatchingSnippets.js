37fbdf8b9898a466e4fe046d60b6d53d727bfe09
Chris Mendoza <realcsmendoza@gmail.com>
2019-05-23 22:35:48 -0700

    index lyrics words with their line numbers

--- server/getMatchingSnippets.js
+++ server/getMatchingSnippets.js
@@ -22,7 +22,40 @@ const getUniqueLyricsLines = (lyricsLines) => {
 	return Array.from(filteringSet);
 };
 
+const getConceptsWithLineNumbers = (lyricsLines, conceptWords) => {
+
+	let indexedWords = new Map(); // match a concept word to its line numbers
+
+	// search lyrics lines for concept words
+	lyricsLines.forEach((lyricsLine, currLineNum) => {
+
+		// grab the current line's words (may contain trailing commas)
+		let rawWords = lyricsLine.split(" ");
+
+		rawWords.forEach(rawWord => {
+
+			word = rawWord.replace(/,/g, ''); // remove any commas attached to word
+
+			// this word matches a concept word
+			if(conceptWords.includes(word)) {
+
+				// this concept has been seen before, update its line numbers
+				if(indexedWords.has(word)) {
+					let lineNums = indexedWords.get(word);
+					lineNums.push(currLineNum); // add the new found line number
+
+					indexedWords.set(word, lineNums); // update the word's line numbers
+				}
+				else // new concept word, map it's first line number
+					indexedWords.set(word, [currLineNum]);
+			}
+		});
+	});
+	return indexedWords;
+}
+
 module.exports = {
+	getConceptsWithLineNumbers,
 	getUniqueLyricsLines,
 	splitLyricsByLine
 };
